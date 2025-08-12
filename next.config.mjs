6a10ae9ee897cd021d57347a81fb26cae59ffb3e
likerang <arang9314@naver.com>
2025-08-12 09:50:47 +0900

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+    domains: ['iittcemymbyjzahdnlnc.supabase.co'],
+  }
+};
+
+export default nextConfig;
