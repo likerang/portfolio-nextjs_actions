ab3caaa026d5d8e012e27313e1c0829054a655b4
likerang <arang9314@naver.com>
2025-08-12 13:22:04 +0900

    LoginStatus.js 추가 (로그아웃 추가)

---
+++ src/component/LoginStatus.js
@@ -0,0 +1,25 @@
+"use client";
+import { createClient } from '@/utils/supabase/client';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function LoginStatus(){
+  const supabase = createClient();
+  const [user, setUser] = useState(null); //로그인한 유저 정보 할당
+  
+  useEffect(()=>{
+    (async ()=>{
+      //유저 정보 조회
+      const { data: { result } } = await supabase.auth.getUser(); 
+      setUser(result);
+      console.log(result);
+    })();
+    console.log(user);
+  },[user, supabase.auth]);
+
+  console.log(user);
+  
+  return(
+    <li><button className="btn btn-primary">Logout</button></li>
+  )
+}
