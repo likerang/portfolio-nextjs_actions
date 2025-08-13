d35c99fb6ce7e91dd32229d0ecc17747ed136d45
likerang <arang9314@naver.com>
2025-08-13 09:46:57 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-    domains: ['iittcemymbyjzahdnlnc.supabase.co'],
-  }
+  output: 'export',
+  basePath: '/portfolio-nextjs_supabase',
+  assetPrefix: '/portfolio-nextjs_supabase/',
+  images: { unoptimized: true }  // Pages에서 권장
 };
 
 export default nextConfig;
